2e4985fa07d57d45e617f54f977c304649fdcc00
David Stakic <davidstakic2003@gmail.com>
2026-04-05 17:25:36 +0200

    [INITIALIZATION] Project Initialization

---
+++ intens/src/main/java/com/example/intens/IntensApplication.java
@@ -0,0 +1,13 @@
+package com.example.intens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntensApplication.class, args);
+	}
+
+}
---
+++ intens/src/test/java/com/example/intens/IntensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
